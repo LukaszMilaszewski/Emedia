1e9832f02613446788ad4c7f2e1485e7b84e9f61
Lukasz Milaszewski <lukasz.milaszewski@gmail.com>
2017-10-22 14:46:23 +0200

    added wav header members

---
+++ emedia/emedia/WAV.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace emedia
+{
+    class WAV
+    {
+        public string chunkID { get; private set; }
+        public int chunkSize { get; private set; }
+        public string format { get; private set; }
+        public string subchunk1ID { get; private set; }
+        public int subchunk1Size { get; private set; }
+        public int audioFormat { get; private set; }
+        public int numChanels { get; private set; }
+        public int sampleRate { get; private set; }
+        public int byteRate { get; private set; }
+        public int blockAlign { get; private set; }
+        public int bitsPerSample { get; private set; }
+        public string subchunk2ID { get; private set; }
+        public int subchunk2Size { get; private set; }
+        public float[,] data { get; private set; }
+        public byte[] dataByte { get; set; }
+        public byte[] headerByte { get; private set; }
+    }
+}
